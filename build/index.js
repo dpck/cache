22e86e723e0194de405042e9a96ce5256dc3797d
Anton <anton@adc.sh>
2019-04-04 20:43:27 +0300

    b

--- build/index.js
+++ build/index.js
@@ -11,21 +11,23 @@ const compare = async (mod, cache = {}, log = console.log) => {
   const current = cache[mod]
   const { mtime, hash } = await analyse(mod)
 
-  let currentMtime, currentHash, crypto
-  if (current) {
-    ({ 'mtime': currentMtime, 'hash': currentHash, 'md5': crypto } = current)
-    const isHashSame = compareHash(currentHash, hash, log)
-    if (isHashSame ) return { result: true, md5: crypto }
-  }
-
   const md5 = createHash('md5').update(JSON.stringify(hash)).digest("hex")
+
   if (!current) return {
     result: false, reason: 'NO_CACHE', mtime, hash, md5,
   }
 
+  const {
+    'mtime': currentMtime,
+    'hash': currentHash,
+  } = current
+
   if (mtime != currentMtime) return {
     result: false, reason: 'MTIME_CHANGE', mtime, hash, currentMtime, md5,
   }
+  const isHashSame = compareHash(currentHash, hash, log)
+  if (isHashSame ) return { result: true, md5 }
+
 
   return { result: false, mtime, hash, reason: 'HASH_CHANGE', md5 }
 }
